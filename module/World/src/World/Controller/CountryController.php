c20e5d80cf8e9e036da6d61a2a3f9fa88186db87
Unkindest <unkindest89@gmail.com>
2013-10-15 20:04:19 +0400

    index Action with Paginator inside Country controller

--- module/World/src/World/Controller/CountryController.php
+++ module/World/src/World/Controller/CountryController.php
@@ -4,15 +4,47 @@ namespace World\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
-class CountryController extends AbstractActionController
-{
+class CountryController extends AbstractActionController {
 
-    public function indexAction()
-    {
-        return new ViewModel();
+    public function indexAction() {
+        $em = $this->getServiceLocator()
+                ->get('doctrine.entitymanager.orm_default');
+        $params = $this->params('id', 0);
+        if ($params) {
+            $arr = explode('&', $params);
+            $maxResult = (int) $arr[0];
+            $page = abs((!empty($arr[1])) ? (int) $arr[1] : 1);
+        }
+        if (empty($maxResult)) {
+            $maxResult = 10;
+            $page = 1;
+            $show = TRUE;
+        } else {
+            $show = FALSE;
+        }
+        $query = $em->createQuery('SELECT u FROM World\Entity\Country u')
+                ->setFirstResult(($page - 1) * $maxResult)
+                ->setMaxResults($maxResult);
+        $paginator = new Paginator($query, $fetchJoinCollection = true);
+        $c = count($paginator);
+        $pages = ceil($c / $maxResult);
+        if (!$show)
+            $this->layout('layout/table');
+
+        $getters = get_class_methods('World\Entity\Country');
+        foreach ($getters as $k => $v)
+            if (substr($v, 0, 3) != 'get')
+                unset($getters[$k]);
+        return new ViewModel(array(
+            'paginator' => $paginator,
+            'count' => $c,
+            'pages' => $pages,
+            'show' => $show,
+            'getters' => $getters,
+        ));
     }
-
 
 }
 
