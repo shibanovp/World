c07a68b8b186b49b4bc0b48c9231398e8be24ae4
Unkindest <unkindest89@gmail.com>
2013-10-15 23:25:28 +0400

    Ajax fix

--- public/World/js/pages.js
+++ public/World/js/pages.js
@@ -1,18 +1,34 @@
 $(function(e) {  
-    $("#tbody").click(function() {$("#tbody").load(BASE_URL +'/world/country/index/10&2'); }); 
+    //$("#tbody").click(function() {$("#tbody").load(BASE_URL +'/world/country/index/10&2'); }); 
     $("li.lipagination").click(function() {
         $("li.lipagination").removeClass('active');
         $(this).addClass("active");
         var page =$(this).attr("value");
         var maxResult = $("#maxResult button.btn.active").attr("value");
         $("#tbody").load(BASE_URL +'/world/country/index/'+maxResult+'&'+page);
-        alert(maxResult); 
+    //    alert(maxResult); 
         });
     $("#maxResult button").click(function() {
         $("#maxResult button").removeClass('active');
         $(this).addClass("active");
+        var maxResults=$(this).attr("value");
+        var pages = Math.ceil(RESULTS/maxResults);
+        $("#ulpagination li").remove();
+        for (var i=1;i<=pages;i++){
+            cname = (1==i)? 'lipagination active' : 'lipagination';
+            li =$("#ulpagination").append('<li class="'+ cname+'" value="'+ i +'"><a>'+i+'</a></li>');
+            $("#tbody").load(BASE_URL +'/world/country/index/'+maxResults+'&'+1);
+        } 
         
-    });    
+    });
+    $(document).on('click',"li.lipagination",function(){
+        //alert('da');
+        $("li.lipagination").removeClass('active');
+        $(this).addClass("active");
+        var page =$(this).attr("value");
+        var maxResult = $("#maxResult button.btn.active").attr("value");
+        $("#tbody").load(BASE_URL +'/world/country/index/'+maxResult+'&'+page);
+    })
 });
 
 /* 
