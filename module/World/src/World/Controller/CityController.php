c4c8bae4fab1313e4505d129739010a9d9c75db1
Unkindest <unkindest89@gmail.com>
2013-10-19 00:33:13 +0400

    CityController right instantiation

--- module/World/src/World/Controller/CityController.php
+++ module/World/src/World/Controller/CityController.php
@@ -7,7 +7,13 @@ use Zend\View\Model\ViewModel;
 
 class CityController extends AbstractActionController
 {
-
+    private $entityManager;
+    public function __construct (\Doctrine\ORM\EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $res = $this->entityManager->getRepository('World\Entity\City')->findAll();
+        var_dump($res);
+    }
     public function indexAction()
     {
         return new ViewModel();
--- public/World/js/googlemap.js
+++ public/World/js/googlemap.js
@@ -1,25 +1,31 @@
 $(function(e) {
    var map;
+function markAddress(address){
+    
+var geocoder = new  google.maps.Geocoder();
+geocoder.geocode( {"address": address} , function(GeocoderResult, GeocoderStatus){
+    //alert(GeocoderResult[0].geometry.bounds);
+    if (GeocoderStatus===google.maps.GeocoderStatus.OK){
+        map.fitBounds(GeocoderResult[0].geometry.bounds);
+        var markerOptions = {
+            position: GeocoderResult[0].geometry.location
+        };
+        var marker = new google.maps.Marker(markerOptions);
+        marker.setMap(map);
+    }
+        
+});
+}
 function initialize() {
   var mapOptions = {
-    zoom: 3,
-    center: new google.maps.LatLng(61.52401, 105.318756),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    zoom: 2,
+    center: new google.maps.LatLng(0, 0),
+   // mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-      
-var markerOptions = {
-    position: new google.maps.LatLng(61.52401, 105.318756)
-};
-var marker = new google.maps.Marker(markerOptions);
-marker.setMap(map);
-var geocoder = new  google.maps.Geocoder();
-geocoder.geocode( {"address": "france"} , function(GeocoderResult, GeocoderStatus){
-    //alert(GeocoderResult[0].geometry.bounds);
-    if (GeocoderStatus===google.maps.GeocoderStatus.OK)
-        map.fitBounds(GeocoderResult[0].geometry.bounds);
-});
+    map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+   markAddress('russia'); 
+    
+
 }
 google.maps.event.addDomListener(window, 'load', initialize); 
 });
